9fa5709f9ae22e2c56990b8a2b86000d8f8897a6
Ramie Mosely <52608883+RamieMosely@users.noreply.github.com>
2024-12-12 17:00:59 -0500

    Finished Letter Class

--- letter.py
+++ letter.py
@@ -1,3 +1,5 @@
+#This is the letter class it contains all the methods and attributes
+
 class Letter:
     def __init__(self, id, first_name, last_name):
         self._id = id
@@ -6,6 +8,7 @@ class Letter:
         self._toys = []
         self._approved = None
 
+#Getter Methods
     def get_id(self):
         return self._id
     
@@ -15,4 +18,17 @@ class Letter:
     def get_last_name(self)
         return self._last_name
     
+    def get_full_name(self):
+        return f"{self._first_name} {self._last_name}"
     
+    def get_toys(self):
+        return self._toys
+    
+    def get_approved(self):
+        return self._approved
+    
+    def set_approved(self, status):
+        self._approved = status
+
+    def add_toy(self, toy):
+        self._toys.append(toy)
