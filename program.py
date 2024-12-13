545157c1b15b2558efef5730722b0a9e8434a003
Ramie Mosely <52608883+RamieMosely@users.noreply.github.com>
2024-12-12 20:44:03 -0500

    Worked on main program

---
+++ program.py
@@ -0,0 +1,13 @@
+#Assignment 3
+#PROG10004
+#Ramie Mosely
+
+import json
+from datetime import datetime
+import csv
+from letter import Letter
+from toy import Toy
+
+class Program:
+    def __init__(self):
+        
